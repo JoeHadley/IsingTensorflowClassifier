43c72dd7fec258fc1ee10f5e80b11036007d4f13
JoeHadley <148891416+JoeHadley@users.noreply.github.com>
2023-12-07 09:58:05 +0000

    Update neuralNet.py
    Changed around to work in VSCode rather than just google colab

--- neuralNet.py
+++ neuralNet.py
@@ -1,182 +1,312 @@
-#Import packages
 import numpy as np
 import tensorflow as tf
-from google.colab import files
 from tensorflow.keras.layers import Flatten, Dense, Dropout
-from sklearn.model_selection import KFold
 from tensorflow.keras.callbacks import EarlyStopping
 
-#Load and preprocess data
-def load_and_preprocess_data(data_path, labels_path, temps_path, num_rows, side_length):
-    with open(data_path, 'rb') as data_file:
-        data = np.fromfile(data_file, dtype=np.int32)
-    data = data.reshape(num_rows, side_length, side_length)
 
-    with open(labels_path, 'rb') as label_file:
-        labels = np.fromfile(label_file, dtype=np.int32)
-    labels = labels.reshape(num_rows, 1)
+def showMe(x,nameString,override=False):
+    print(nameString)
 
-    with open(labels_path, 'rb') as temps_file:
-        temps = np.fromfile(temps_file, dtype=np.float64)
-    temps = temps.reshape(num_rows, 1)
+    print(x)
+    print('uniques: ', np.unique(x))
+    print('length: ', len(x))
+    print('size: ', np.size(x))
+    print('average: ', np.mean(x))
+    
 
-    indices = np.random.permutation(num_rows)
-    data = data[indices]
-    labels = labels[indices]
-    temps = temps[indices]
+# Load and preprocess data
+def load_and_preprocess_data(data_path, labels_path, num_rows, side_length):
+    data = np.fromfile(data_path, dtype=np.int32).reshape(num_rows, side_length, side_length)
+    labels = np.fromfile(labels_path, dtype=np.int32).reshape(num_rows, 1)
+    return data, labels
 
-    return data, labels, temps
+def shuffle_data(data, labels):
+    indices = np.random.permutation(len(labels))
+    return data[indices], labels[indices]
 
-# Load and preprocess training and testing data
 
-training_data_number = 2500
+def getModelData(model):
 
-#training_data, training_labels = load_and_preprocess_data('TrainingData.dat', 'TrainingLabels.dat', 1000, 10)
-#testing_data, testing_labels = load_and_preprocess_data('TestingData.dat', 'TestingLabels.dat', 300, 10)
-training_data, training_labels, training_temps = load_and_preprocess_data('TrainingData.dat', 'TrainingLabels.dat', 'TrainingTemps.dat', 2500, 10)
 
-# Define the neural network model
+
+    model_training_labels = np.concatenate((training_labels[:model*size], training_labels[(model+1)*size:]), axis=0)
+    model_testing_labels = training_labels[model*size:(model+1)*size]
+
+    model_training_data = np.concatenate((training_data[:model*size, :, :], training_data[(model+1)*size:, :, :]), axis=0)
+    model_testing_data = training_data[model*size:(model+1)*size, :, :]
+
+    return model_training_labels, model_testing_labels, model_training_data, model_testing_data
+
+
+def get_integers_except_n(start, end, n):
+    return [i for i in range(start, end + 1) if i != n]
+
 def create_model():
     model = tf.keras.models.Sequential([
-        Flatten(input_shape=(10, 10)),
+        Flatten(input_shape=(side_length, side_length)),
         Dense(128, activation='relu'),
         Dropout(0.2),
-        Dense(2)
+        Dense(output_node_number)
     ])
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
     return model
 
+# Load and preprocess training and validating data
+temperature_number = 50
+sample_number = 50
+side_length = 10
+training_data_number = temperature_number * sample_number
+min_temp = 1
+max_temp = 5
+mode = 1 # Mode 1 - binary labels, Mode 2 - temperature labels
 
+
+print('Got here!')
+
+
+training_data_string = 'Training'
+validating_data_string = 'Validating'
+
+if mode == 1: # Use binary labels 
+    training_data, training_labels = load_and_preprocess_data(training_data_string+'Data.dat', training_data_string+'Blabels.dat', training_data_number, side_length)
+    training_data, training_labels = shuffle_data(training_data, training_labels)
+    validation_data, validation_labels = load_and_preprocess_data(validating_data_string+'Data.dat', validating_data_string+'Blabels.dat', training_data_number, side_length)
+    output_node_number = 2
+
+else: # Use temperature labels
+    training_data, training_labels = load_and_preprocess_data(training_data_string+'Data.dat', training_data_string+'Tlabels.dat', training_data_number, side_length)
+    training_data, training_labels = shuffle_data(training_data, training_labels)
+    validation_data, validation_labels = load_and_preprocess_data(validating_data_string+'Data.dat', validating_data_string+'Tlabels.dat', training_data_number, side_length)
+    # Recover temperatures from temperature labels
+    training_temps = min_temp + training_labels*(max_temp-min_temp)/(temperature_number-1)
+    validation_temps = min_temp + validation_labels*(max_temp-min_temp)/(temperature_number-1)
+    output_node_number = temperature_number
+
+
+
+
+
+
+
+# Define the neural network model
 
 
 early_stopping = EarlyStopping(
-  monitor='val_loss',     # Monitor validation loss
-  patience=3,             # Stop after 3 epochs with no improvement
-  restore_best_weights=True  # Restore the best model weights
+    monitor='val_loss',
+    patience=3,
+    restore_best_weights=True
 )
 
 
+
 # We will train 5 models on different parts of the data.
+modelNumber = 5
+size = training_data_number // modelNumber  # Use integer division to ensure an integer size
 
-size = training_data_number // 5  # Use integer division to ensure an integer size
+training_labels = training_labels.flatten()
 
-tempChunk1 = training_labels[0:size - 1]
-tempChunk2 = training_labels[size:2 * size - 1]
-tempChunk3 = training_labels[2 * size:3 * size - 1]
-tempChunk4 = training_labels[3 * size:4 * size - 1]
-tempChunk5 = training_labels[4 * size:5 * size - 1]
-NN1TrainingTemps = np.concatenate((tempChunk1,tempChunk2,tempChunk3,tempChunk4),axis = 0)
-NN1TestingTemps = tempChunk5
-NN2TrainingTemps = np.concatenate((tempChunk1,tempChunk2,tempChunk3,tempChunk5),axis = 0)
-NN2TestingTemps = tempChunk4
-NN3TrainingTemps = np.concatenate((tempChunk1,tempChunk2,tempChunk4,tempChunk5),axis = 0)
-NN3TestingTemps = tempChunk3
-NN4TrainingTemps = np.concatenate((tempChunk1,tempChunk3,tempChunk4,tempChunk5),axis = 0)
-NN4TestingTemps = tempChunk2
-NN5TrainingTemps = np.concatenate((tempChunk2,tempChunk3,tempChunk4,tempChunk5),axis = 0)
-NN5TestingTemps = tempChunk1
-
-
-
-labelChunk1 = training_labels[0:size - 1]
-labelChunk2 = training_labels[size:2 * size - 1]
-labelChunk3 = training_labels[2 * size:3 * size - 1]
-labelChunk4 = training_labels[3 * size:4 * size - 1]
-labelChunk5 = training_labels[4 * size:5 * size - 1]
-NN1TrainingLabels = np.concatenate((labelChunk1,labelChunk2,labelChunk3,labelChunk4),axis = 0)
-NN1TestingLabels = labelChunk5
-NN2TrainingLabels = np.concatenate((labelChunk1,labelChunk2,labelChunk3,labelChunk5),axis = 0)
-NN2TestingLabels = labelChunk4
-NN3TrainingLabels = np.concatenate((labelChunk1,labelChunk2,labelChunk4,labelChunk5),axis = 0)
-NN3TestingLabels = labelChunk3
-NN4TrainingLabels = np.concatenate((labelChunk1,labelChunk3,labelChunk4,labelChunk5),axis = 0)
-NN4TestingLabels = labelChunk2
-NN5TrainingLabels = np.concatenate((labelChunk2,labelChunk3,labelChunk4,labelChunk5),axis = 0)
-NN5TestingLabels = labelChunk1
-
-
-dataChunk1 = training_data[0:size - 1]
-dataChunk2 = training_data[size:2 * size - 1]
-dataChunk3 = training_data[2 * size:3 * size - 1]
-dataChunk4 = training_data[3 * size:4 * size - 1]
-dataChunk5 = training_data[4 * size:5 * size - 1]
-
-NN1TrainingData = np.concatenate((dataChunk1,dataChunk2,dataChunk3,dataChunk4),axis = 0)
-NN1TestingData = dataChunk5
-NN2TrainingData = np.concatenate((dataChunk1,dataChunk2,dataChunk3,dataChunk5),axis = 0)
-NN2TestingData = dataChunk4
-NN3TrainingData = np.concatenate((dataChunk1,dataChunk2,dataChunk4,dataChunk5),axis = 0)
-NN3TestingData = dataChunk3
-NN4TrainingData = np.concatenate((dataChunk1,dataChunk3,dataChunk4,dataChunk5),axis = 0)
-NN4TestingData = dataChunk2
-NN5TrainingData = np.concatenate((dataChunk2,dataChunk3,dataChunk4,dataChunk5),axis = 0)
-NN5TestingData = dataChunk1
 
 
 # Train the model
-
-model1 = tf.keras.models.Sequential([Flatten(input_shape=(10, 10)),Dense(128, activation='relu'),Dropout(0.2),Dense(2)])
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model1.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
 
-model2 = tf.keras.models.Sequential([Flatten(input_shape=(10, 10)),Dense(128, activation='relu'),Dropout(0.2),Dense(2)])
+
+
+#models = [create_model() for _ in range(modelNumber)]
+
+#for i, model in enumerate(models):
+#    model_training_labels, model_testing_labels, model_training_data, model_testing_data = getModelData(i)
+#    model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+
+
+
+
+
+neuralNet = tf.keras.models.Sequential([
+    Flatten(input_shape=(side_length, side_length)),
+    Dense(128, activation='relu'),
+    Dropout(0.2),
+    Dense(output_node_number)
+])
+
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model2.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
-
-model3 = tf.keras.models.Sequential([Flatten(input_shape=(10, 10)),Dense(128, activation='relu'),Dropout(0.2),Dense(2)])
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model3.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
-
-model4 = tf.keras.models.Sequential([Flatten(input_shape=(10, 10)),Dense(128, activation='relu'),Dropout(0.2),Dense(2)])
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model4.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
-
-model5 = tf.keras.models.Sequential([Flatten(input_shape=(10, 10)),Dense(128, activation='relu'),Dropout(0.2),Dense(2)])
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model5.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+neuralNet.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
 
 
+model = 0
+
+model_training_labels = np.concatenate((training_labels[:model*size], training_labels[(model+1)*size:]), axis=0)
+model_testing_labels = training_labels[model*size:(model+1)*size]
+
+model_training_data = np.concatenate((training_data[:model*size, :, :], training_data[(model+1)*size:, :, :]), axis=0)
+model_testing_data = training_data[model*size:(model+1)*size, :, :]
+
+print(model_testing_data)
 
 
-
-model1.fit(NN1TrainingData, NN1TrainingLabels, epochs=50, validation_data=(NN1TestingData, NN1TestingLabels), callbacks=[early_stopping])
-model2.fit(NN2TrainingData, NN2TrainingLabels, epochs=50, validation_data=(NN2TestingData, NN2TestingLabels), callbacks=[early_stopping])
-model3.fit(NN3TrainingData, NN3TrainingLabels, epochs=50, validation_data=(NN3TestingData, NN3TestingLabels), callbacks=[early_stopping])
-model4.fit(NN4TrainingData, NN4TrainingLabels, epochs=50, validation_data=(NN4TestingData, NN4TestingLabels), callbacks=[early_stopping])
-model5.fit(NN5TrainingData, NN5TrainingLabels, epochs=50, validation_data=(NN5TestingData, NN5TestingLabels), callbacks=[early_stopping])
+'''
+neuralNet.fit(model_training_data, model_training_labels, epochs=50, validation_data=(model_testing_data, model_testing_labels), callbacks=[early_stopping])
+'''
 
 
 
 
-validation_data, validation_labels, validation_temps = load_and_preprocess_data('ValidationData.dat', 'ValidationLabels.dat', 'ValidationTemps.dat', 2500, 10)
+
 
 validation_labels = validation_labels.flatten()
-validation_temps = validation_temps.flatten()
+
+#showMe(validation_data[600])
+'''
+print(validation_data[600])
+print(validation_labels[600])
+
+
+print(sum(sum(validation_data[1000]))/100)
+'''
+
+
+'''
+critTemp = 2.269185
+badRowArray = np.array([])
+badLabelArray = np.array([])
+badTempArray = np.array([])
+threshold = 0.5
+
+dataArray = 2*validation_data - 1
+labelArray = validation_labels
+
+if mode == 2:
+
+    tempArray = training_temps
 
 
 
-guesses1 = []
-
-for t in range(5):
-    for sample in range(5):
-        index = t * 5 + sample
-        prediction1 = model1.predict(validation_data[index:index+1])
-        guesses1.append(np.argmax(prediction1, axis=1))
-
-guesses1 = np.array(guesses1).flatten()
-
-accuracies = []
-temperatures = []
-
-for t in range(5):
-    correctGuesses = 0
-    for sample in range(5):
-        index = t * 5 + sample
-        if guesses1[index] == validation_labels[index]:
-            correctGuesses += 1
-    accuracy = correctGuesses / 50
-    accuracies.append(accuracy)
-    temperatures.append(validation_temps[t * 50])
+for i in range(training_data_number):
+    badness = 0
+    badnessString = 'fine'
+    magnetization = np.mean(dataArray[i])
 
     
+    if mode == 2:
+
+        if tempArray < critTemp:
+
+            tempString = 'cold'
+            
+            if abs(magnetization) < threshold:
+
+                badness = 1
+                badnessString = 'bad'
+                badRowArray = np.append(badRowArray,i)
+                badLabelArray = np.append(badLabelArray,labelArray[i])
+
+
+        elif  tempArray > critTemp:
+
+            tempString = 'hot'
+
+            if abs(magnetization) > threshold:
+
+                badness = 1
+                badnessString = 'bad'
+                badRowArray = np.append(badRowArray,i)
+                badLabelArray = np.append(badLabelArray,labelArray[i])
+
+    else:
+
+        if labelArray[i] == 0:
+
+            tempString = 'cold'
+            
+            if abs(magnetization) < threshold:
+
+                badness = 1
+                badnessString = 'bad'
+                badRowArray = np.append(badRowArray,i)
+                badLabelArray = np.append(badLabelArray,labelArray[i])
+
+
+        else:
+
+            tempString = 'hot'
+
+            if abs(magnetization) > threshold:
+
+                badness = 1
+                badnessString = 'bad'
+                badRowArray = np.append(badRowArray,i)
+                badLabelArray = np.append(badLabelArray,labelArray[i])
+
+
+
+    showString = 'Row ' + str(i) + ', label ' +  str(labelArray[i]) + ', lattice is '  + tempString + ', mean is ' + str(magnetization) + ', this is ' + badnessString
+    print(showString)
+
+
+#showMe(badLabelArray)
+#showMe(badTempArray)
+
+
+showMe(badRowArray,'badRowArray',True)
+print('uniques: ', np.unique(badRowArray))
+
+showMe(dataArray[860],'Validation 860')
+print(validation_labels[860])
+
+
+'''
+
+
+big_accuracies = np.array([]).reshape(0, temperature_number)  # Start with an empty array with the right shape
+temperatures = np.unique(validation_temps)
+
+
+
+temperature_total_guesses = np.zeros(temperature_number)
+temperature_right_guesses = np.zeros(temperature_number)
+temperature_nearly_right_guesses = np.zeros(temperature_number)
+
+
+
+#for i, model in enumerate(models):
+model_guesses = []
+model_accuracies = []
+
+for t in range(temperature_number):
+    for sample in range(sample_number):
+        index = t * sample_number + sample
+        
+        correct_label = validation_labels[index]
+
+        print('Correct label is ',correct_label)
+        temperature_total_guesses[correct_label] = temperature_total_guesses[correct_label] + 1
+
+
+        
+        model_prediction = model0.predict(validation_data[index:index+1],verbose=0) # sus
+        guessed_label = np.argmax(model_prediction, axis=1)
+        print('Model ' + str(model) + ', Temperature ' + str(t) + ', sample ' + str(sample) + ' guessed ' + str(guessed_label))
+
+        if guessed_label == correct_label:
+            temperature_right_guesses[correct_label] = temperature_right_guesses[correct_label] + 1
+
+print(temperature_right_guesses/temperature_total_guesses)
+    
+
+
+
+
+
+
+
+
+means = []
+errors = []
+
+for t in range(temperature_number):
+    mean = np.mean(big_accuracies[:,t])
+    error = np.std(big_accuracies[:,t])/np.sqrt(modelNumber)
+
+    means.append(mean)
+    errors.append(error)
